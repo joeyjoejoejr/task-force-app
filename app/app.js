42a5b5e62e7cc3721e3af26bd3521386302d65e4
Joe Jackson <cpmhjoe@gmail.com>
2015-08-02 13:52:58 -0400

    Initial

---
+++ app/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import TaskForceApp from "./components/TaskForceApp.js";
+
+React.render(
+  <TaskForceApp />,
+  document.body
+)
---
+++ app/components/TaskForceApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class TaskForceApp extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Task Force App</h1>
+      </div>
+    );
+  }
+}
